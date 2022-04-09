20e1bd96013fe1aae40ffbf04e0fe73ca57ddd65
createvaluefor <2019143077@yonsei.ac.kr>
2022-04-09 12:50:50 +0900

    ipad bersion

--- routes/history.js
+++ routes/history.js
@@ -17,19 +17,25 @@ router.get('/seoul/:duration', async (req, res) => {
     const duration = req.params.duration
 
     try {
-        const isExist = await fs.existsSync(`./public/history/연혁별/서울캠퍼스/${duration}년`)
-        if (!isExist) {
+        const isExist = await fs.existsSync(`./public/history/original연혁별/서울캠퍼스/${duration}년`)
+        const isIpadExist = await fs.existsSync(`./public/history/ipad/연혁별/서울캠퍼스/${duration}년`)
+        if (!isExist || !isIpadExist) {
             return res.status(400).json({
                 success: false,
                 message: '해당 년도의 연혁 사진이 존재하지 않습니다.'
             })
         }
-        const dir = await fs.readdirSync(`./public/history/연혁별/서울캠퍼스/${duration}년`).map(item =>
-            `${PUBLIC_URL}서울캠퍼스/${duration}년/${item}`)
+        const dir = await fs.readdirSync(`./public/history/original/연혁별/서울캠퍼스/${duration}년`).map(item =>
+            `${PUBLIC_URL}original/서울캠퍼스/${duration}년/${item}`)
+        const ipadDir = await fs.readdirSync(`./public/history/history/연혁별/서울캠퍼스/${duration}년`).map(item =>
+            `${PUBLIC_URL}history/서울캠퍼스/${duration}년/${item}`)
         res.json({
             success: true,
             message: duration + "년의 연혁 사진을 정상적으로 불러왔습니다.",
-            data: dir
+            data: {
+                original: dir,
+                ipad: ipadDir
+            }
 
         })
     } catch (err) {
@@ -46,19 +52,22 @@ router.get('/sejong/:duration', async (req, res) => {
     const duration = req.params.duration
 
     try {
-        const isExist = await fs.existsSync(`./public/history/연혁별/세종캠퍼스/${duration}년`)
-        if (!isExist) {
+        const isExist = await fs.existsSync(`./public/history/original/연혁별/세종캠퍼스/${duration}년`)
+        const isIpadExist = await fs.existsSync(`./public/history/ipad/연혁별/세종캠퍼스/${duration}년`)
+        if (!isExist || !isIpadExist) {
             return res.status(400).json({
                 success: false,
                 message: '해당 년도의 연혁 사진이 존재하지 않습니다.'
             })
         }
-        const dir = await fs.readdirSync(`./public/history/연혁별/세종캠퍼스/${duration}년`).map(item =>
-            `${PUBLIC_URL}세종캠퍼스/${duration}년/${item}`)
+        const dir = await fs.readdirSync(`./public/history/origianl/연혁별/세종캠퍼스/${duration}년`).map(item =>
+            `${PUBLIC_URL}original/세종캠퍼스/${duration}년/${item}`)
+        const ipadDir = await fs.readdirSync(`./public/history/ipad/연혁별/세종캠퍼스/${duration}년`).map(item =>
+            `${PUBLIC_URL}ipad/세종캠퍼스/${duration}년/${item}`)
         res.json({
             success: true,
             message: duration + "년의 연혁 사진을 정상적으로 불러왔습니다.",
-            data: dir
+            data: { original: dir, ipad: ipadDir }
 
         })
     } catch (err) {
@@ -73,25 +82,30 @@ router.get('/sejong/:duration', async (req, res) => {
 //테마별 메인 사진 불러오기
 router.get('/theme', async (req, res) => {
 
-    const BASIC_PATH = `${PUBLIC_URL}테마별/`
+    const BASIC_PATH = `${PUBLIC_URL}original/테마별/`
+    const IPAD_PATH = `${PUBLIC_URL}ipad/테마별/`
 
-    const dir = await fs.readdirSync(`./public/history/테마별`).filter(item =>
+    const dir = await fs.readdirSync(`./public/history/original/테마별`).filter(item =>
+        item.includes('jpg')).map(item => BASIC_PATH + item)
+    const ipadDir = await fs.readdirSync(`./public/history/ipad/테마별`).filter(item =>
         item.includes('jpg')).map(item => BASIC_PATH + item)
 
     res.json({
         success: true,
         message: "테마를 정상적으로 불러왔습니다.",
-        data: dir
+        data: { original: dir, ipad: ipadDir }
     })
 })
 
 //테마별 불러오기
 router.get('/theme/:theme', async (req, res) => {
     const theme = req.params.theme
-    const BASIC_PATH = `${PUBLIC_URL}테마별/${theme}/`
+    const BASIC_PATH = `${PUBLIC_URL}original/테마별/${theme}/`
+    const IPAD_PATH = `${PUBLIC_URL}ipad/테마별/${theme}/`
 
-    const isExist = await fs.existsSync(`./public/history/테마별/${theme}`)
-    if (!isExist) {
+    const isExist = await fs.existsSync(`./public/history/original/테마별/${theme}`)
+    const isIpadExist = await fs.existsSync(`./public/history/ipad/테마별/${theme}`)
+    if (!isExist || !isIpadExist) {
         return res.status(400).json({
             success: false,
             message: '해당 테마가 존재하지 않습니다. 오타가 있는지 확인해주세요.'
@@ -99,13 +113,18 @@ router.get('/theme/:theme', async (req, res) => {
     }
 
 
-    const dir = await fs.readdirSync(`./public/history/테마별/${theme}`)
+    const dir = await fs.readdirSync(`./public/history/original/테마별/${theme}`)
+        .map(item => BASIC_PATH + item)
+    const ipadDir = await fs.readdirSync(`./public/history/ipad/테마별/${theme}`)
         .map(item => BASIC_PATH + item)
 
     res.json({
         success: true,
         message: theme + " 테마를 정상적으로 불러왔습니다.",
-        data: dir
+        data: {
+            original: dir,
+            ipad: ipadDir
+        }
     })
 })
 
@@ -113,29 +132,45 @@ router.get('/theme/:theme', async (req, res) => {
 router.get('/people/great/:name', async (req, res) => {
 
     const name = req.params.name
-    const BASIC_PATH = `${PUBLIC_URL}인물별/위인/${name}/`
+    const BASIC_PATH = `${PUBLIC_URL}original/인물별/위인/${name}/`
+    const IPAD_PATH = `${PUBLIC_URL}ipad/인물별/위인/${name}/`
     try {
-        const dir = fs.readdirSync(`./public/history/인물별/위인`).filter(item => {
+        const dir = fs.readdirSync(`./public/history/original/인물별/위인`).filter(item => {
             console.log(item, name, item === name)
-
+        })
+        const ipadDir = fs.readdirSync(`./public/history/ipad/인물별/위인`).filter(item => {
+            console.log(item, name, item === name)
         })
         console.log(dir)
 
 
-        const img = fs.readdirSync(`./public/history/인물별/위인/${name}`).filter(item => item.includes('png')).map(item => ({
+        const img = fs.readdirSync(`./public/history/original/인물별/위인/${name}`).filter(item => item.includes('png')).map(item => ({
 
             img: (BASIC_PATH + item),
             folder: item.replace(/[^\uAC00-\uD7AF\u1100-\u11FF\u3130-\u318F]/gi, "")
             // folder: item.replace(/[^\uAC00-\uD7AF\u1100-\u11FF\u3130-\u318F]/gi, "")
         }))
-        const description = fs.readFileSync(`./public/history/인물별/위인/${name}/${name}.txt`).toString()
+        const ipadImg = fs.readdirSync(`./public/history/ipad/인물별/위인/${name}`).filter(item => item.includes('png')).map(item => ({
+
+            img: (BASIC_PATH + item),
+            folder: item.replace(/[^\uAC00-\uD7AF\u1100-\u11FF\u3130-\u318F]/gi, "")
+            // folder: item.replace(/[^\uAC00-\uD7AF\u1100-\u11FF\u3130-\u318F]/gi, "")
+        }))
+        const description = fs.readFileSync(`./public/history/original/인물별/위인/${name}/${name}.txt`).toString()
+        const ipadDescription = fs.readFileSync(`./public/history/ipad/인물별/위인/${name}/${name}.txt`).toString()
 
         res.json({
             success: true,
             message: "유저 정보를 정상적으로 불러왔습니다.",
             data: {
-                description,
-                img,
+                original: {
+                    description,
+                    img,
+                },
+                ipad: {
+                    img: ipadImg,
+                    description: ipadDescription
+                }
 
             }
         })
@@ -146,51 +181,28 @@ router.get('/people/great/:name', async (req, res) => {
         })
     }
 })
-// // 인물별 위인 불러오기
-// router.get('/people/great/:name', async (req, res) => {
-//     console.log('asdasdf')
 
-//     const name = req.params.name
-//     const BASIC_PATH = `${PUBLIC_URL}인물별/위인/${name}/`
-//     try {
-
-//         const img = fs.readdirSync(`./public/history/인물별/위인/${name}`).filter(item => item.includes('png')).map(item => BASIC_PATH + item)
-
-
-//         const description = fs.readFileSync(`./public/history/인물별/위인/${name}/${name}.txt`).toString()
-//         res.json({
-//             success: true,
-//             message: "유저 정보를 정상적으로 불러왔습니다.",
-//             data: {
-//                 description,
-//                 img,
-//                 titles
-
-//             }
-//         })
-//     } catch (err) {
-//         res.json({
-//             success: false,
-//             error: err
-//         })
-//     }
-// })
 
 //인물별 이수현 특별히...
 router.get('/people/great/:name/:title', async (req, res) => {
 
     const { name, title } = req.params
-    const BASIC_PATH = `${PUBLIC_URL}인물별/위인/${name}/${title}/`
+    const BASIC_PATH = `${PUBLIC_URL}original/인물별/위인/${name}/${title}/`
+    const IPAD_PATH = `${PUBLIC_URL}ipad/인물별/위인/${name}/${title}/`
     try {
 
-        const img = fs.readdirSync(`./public/history/인물별/위인/${name}/${title}`).filter(item => item.includes('png') || item.includes('jpg') || item.includes('JPG')).map(item => BASIC_PATH + item)
+        const img = fs.readdirSync(`./public/history/original/인물별/위인/${name}/${title}`).filter(item => item.includes('png') || item.includes('jpg') || item.includes('JPG')).map(item => BASIC_PATH + item)
+        const ipadImg = fs.readdirSync(`./public/history/ipad/인물별/위인/${name}/${title}`).filter(item => item.includes('png') || item.includes('jpg') || item.includes('JPG')).map(item => IPAD_PATH + item)
 
         res.json({
             success: true,
             message: "유저 정보를 정상적으로 불러왔습니다.",
             data:
 
-                img
+            {
+                original: img,
+                ipad: ipadImg
+            }
 
         })
     } catch (err) {
@@ -204,25 +216,30 @@ router.get('/people/great/:name/:title', async (req, res) => {
 //인물별 불러오기
 router.get('/people/:nick', async (req, res) => {
     const nick = req.params.nick
-    const BASIC_PATH = `${PUBLIC_URL}인물별/${nick}/`
+    const BASIC_PATH = `${PUBLIC_URL}original/인물별/${nick}/`
+    const IPAD_PATH = `${PUBLIC_URL}ipad/인물별/${nick}/`
     if (nick === '위인') {
-        const dir = await fs.readdirSync(`./public/history/인물별/위인`)
+        const dir = await fs.readdirSync(`./public/history/original/인물별/위인`)
+        const ipadDr = await fs.readdirSync(`./public/history/ipad/인물별/위인`)
         return res.json({
             success: true,
             message: '위인 리스트',
-            data: dir
+            data: { original: dir, ipad: ipadDr }
         })
     } else {
-        console.log(BASIC_PATH)
-        const isExist = await fs.existsSync(`./public/history/인물별/${nick}`)
-        if (!isExist) {
+
+        const isExist = await fs.existsSync(`./public/history/original/인물별/${nick}`)
+        const isIpadExist = await fs.existsSync(`./public/history/ipad/인물별/${nick}`)
+        if (!isExist || !isIpadExist) {
             return res.status(400).json({
                 success: false,
                 message: '오타가 있는지 확인해주세요.'
             })
         }
-        const dir = await fs.readdirSync(`./public/history/인물별/${nick}`)
+        const dir = await fs.readdirSync(`./public/history/original/인물별/${nick}`)
             .map(item => BASIC_PATH + item)
+        const dir = await fs.readdirSync(`./public/history/ipad/인물별/${nick}`)
+            .map(item => IPAD_PATH + item)
         return res.json({
             success: true,
             message: nick + " 사진을 정상적으로 불러왔습니다.",
@@ -237,17 +254,24 @@ router.get('/people/:nick', async (req, res) => {
 
 router.get('/title/:title', async (req, res) => {
     const title = req.params.title
-    const BASE_PATH = `./public/history/주제별/${title}`
+    const BASE_PATH = `./public/history/original/주제별/${title}`
+    const IPAD_PATH = `./public/history/ipad/주제별/${title}`
     if (['교우회', '역대 교사', '스포츠'].includes(title)) {
         try {
             const data = fs.readdirSync(BASE_PATH).map(item => {
-                return `${PUBLIC_URL}주제별/${title}/${item}`
+                return `${PUBLIC_URL}original/주제별/${title}/${item}`
+            })
+            const ipadData = fs.readdirSync(IPAD_PATH).map(item => {
+                return `${PUBLIC_URL}ipad/주제별/${title}/${item}`
             })
 
             return res.json({
                 success: true,
                 message: title + " 사진들을 정상적으로 불러왔습니다.",
-                data
+                data: {
+                    original: data,
+                    ipad: ipadData
+                }
             })
         } catch (error) {
             return res.json({
@@ -268,8 +292,10 @@ router.get("/title/:title/:subtitle", async (req, res) => {
     const { title, subtitle } = req.params
     if (['학생활동', '축제', '고연전'].includes(title)) {
         try {
-            const data = fs.readdirSync(`./public/history/주제별/${title}/${subtitle}`)
-                .map(item => `${PUBLIC_URL}주제별/${title}/${subtitle}/${item}`)
+            const data = fs.readdirSync(`./public/history/original/주제별/${title}/${subtitle}`)
+                .map(item => `${PUBLIC_URL}original/주제별/${title}/${subtitle}/${item}`)
+            const ipadData = fs.readdirSync(`./public/history/ipad/주제별/${title}/${subtitle}`)
+                .map(item => `${PUBLIC_URL}ipad/주제별/${title}/${subtitle}/${item}`)
             return res.json({
                 success: true,
                 message: `${subtitle} 사진들을 정상적으로 불러왔습니다.`,
@@ -314,15 +340,27 @@ router.get("/star/:genre/:name", async (req, res) => {
             raw: true,
             nest: true
         })
-        const basePath = `./public/history/주제별/스포츠 스타/${genre}/${name}/`;
+        const basePath = `./public/history/original/주제별/스포츠 스타/${genre}/${name}/`;
         const thumb = await fs.readdirSync(basePath + '대표사진').map(item => {
-            return `${PUBLIC_URL}주제별/스포츠 스타/${genre}/${name}/대표사진/${item}`
+            return `${PUBLIC_URL}original/주제별/스포츠 스타/${genre}/${name}/대표사진/${item}`
         })
         const thumb_detail = await fs.readdirSync(basePath + "추가사진").map(item => {
-            return `${PUBLIC_URL}주제별/스포츠 스타/${genre}/${name}/추가사진/${item}`
+            return `${PUBLIC_URL}original/주제별/스포츠 스타/${genre}/${name}/추가사진/${item}`
         })
         const thumb_thumb_detail = await fs.readdirSync(basePath + "추가사진").map(item => {
-            return `${PUBLIC_URL}주제별/스포츠 스타/${genre}/${name}/thumbnail/${item}`
+            return `${PUBLIC_URL}original/주제별/스포츠 스타/${genre}/${name}/thumbnail/${item}`
+        })
+
+
+        const ipadBasePath = `./public/history/ipad/주제별/스포츠 스타/${genre}/${name}/`;
+        const ipadThumb = await fs.readdirSync(basePath + '대표사진').map(item => {
+            return `${PUBLIC_URL}ipad/주제별/스포츠 스타/${genre}/${name}/대표사진/${item}`
+        })
+        const ipad_thumb_detail = await fs.readdirSync(basePath + "추가사진").map(item => {
+            return `${PUBLIC_URL}ipad/주제별/스포츠 스타/${genre}/${name}/추가사진/${item}`
+        })
+        const ipad_thumb_thumb_detail = await fs.readdirSync(basePath + "추가사진").map(item => {
+            return `${PUBLIC_URL}ipad/주제별/스포츠 스타/${genre}/${name}/thumbnail/${item}`
         })
 
 
@@ -330,10 +368,18 @@ router.get("/star/:genre/:name", async (req, res) => {
             success: true,
             message: genre + " 선수들을 정상적으로 불러왔습니다.",
             data: {
-                ...star,
-                thumb,
-                thumb_detail,
-                thumb_thumb_detail
+                original: {
+                    ...star,
+                    thumb,
+                    thumb_detail,
+                    thumb_thumb_detail
+                },
+                ipad: {
+                    ...star,
+                    ipadThumb,
+                    ipad_thumb_detail,
+                    ipad_thumb_thumb_detail
+                }
             }
         })
     } catch (err) {
